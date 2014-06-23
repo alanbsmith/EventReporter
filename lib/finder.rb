c91b844b6865d456146a1f742378f2e55172b221
Jonmichael Chambers <jon@tyrbo.net>
2014-06-23 13:30:17 -0600

    Implements Finder and tests

--- lib/finder.rb
+++ lib/finder.rb
@@ -4,4 +4,13 @@ class Finder
   def initialize(entries)
     @entries = entries
   end
+
+  def find(opts = {})
+    results = entries
+    opts.each do |key, value|
+      value = Array(value)
+      results = results.select { |entry| value.include?(entry[key]) }
+    end
+    results
+  end
 end
--- test/finder_test.rb
+++ test/finder_test.rb
@@ -4,9 +4,10 @@ require './lib/finder'
 class FinderTest < MiniTest::Test
   def entries
     [
+      { first_name: 'John', last_name: 'Doe' },
+      { first_name: 'Alex', last_name: 'Johnson' },
       { first_name: 'John', last_name: 'Smith' },
-      { first_name: 'Jane', last_name: 'Doe' },
-      { first_name: 'Alex', last_name: 'Johnson' }
+      { first_name: 'Jane', last_name: 'Doe' }
     ]
   end
 
@@ -17,5 +18,36 @@ class FinderTest < MiniTest::Test
   def test_it_accepts_an_array_of_entries
     f = Finder.new(entries)
     assert_equal entries, f.entries
+  end
+
+  def test_it_can_find_by_key_and_value
+    f = Finder.new(entries)
+    result = f.find(first_name: 'Alex')
+    assert_equal [{first_name: 'Alex', last_name: 'Johnson'}], result
+    
+    result = f.find(first_name: 'John').sort_by { |e| e[:last_name] }
+    assert_equal [{first_name: 'John', last_name: 'Doe'}, {first_name: 'John', last_name: 'Smith'}], result
+  end
+
+  def test_it_can_find_by_multiple_values
+    f = Finder.new(entries)
+    result = f.find(first_name: ['Jane', 'Alex']).sort_by { |e| e[:last_name] }
+    assert_equal [{first_name: 'Jane', last_name: 'Doe'}, {first_name: 'Alex', last_name: 'Johnson'}], result
+  end
+
+  def test_it_can_find_by_multiple_columns
+    f = Finder.new(entries)
+    result = f.find(first_name: 'John', last_name: 'Doe')
+    assert_equal [{first_name: 'John', last_name: 'Doe'}], result
+  end
+
+  def test_it_can_find_by_multiple_columns_and_values
+    f = Finder.new(entries)
+    result = f.find(first_name: ['John', 'Jane'], last_name: ['Doe', 'Smith']).sort_by { |e| e[:last_name] }
+    assert_equal [
+      { first_name: 'John', last_name: 'Doe' },
+      { first_name: 'Jane', last_name: 'Doe' },
+      { first_name: 'John', last_name: 'Smith' }
+    ], result
   end
 end
