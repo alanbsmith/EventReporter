c68fcd8d135825142298925e04f3e2d81a56fb94
Jonmichael Chambers <jon@tyrbo.net>
2014-06-24 14:37:19 -0600

    Finishes FindCommand stuff

--- lib/commands/find_command.rb
+++ lib/commands/find_command.rb
@@ -1,6 +1,8 @@
 require './lib/command'
 
 class FindCommand < Command
+  attr_reader :hash
+
   def self.aliases
     ['f', 'find']
   end
@@ -14,8 +16,34 @@ class FindCommand < Command
   def initialize(obj, args)
     @obj = obj
     @args = args
+    @hash = {}
   end
 
   def execute
+  end
+
+  def parse_args
+    groups = args.split('and').map(&:strip)
+    groups.each do |group|
+      handle_group(group)
+    end
+    hash
+  end
+
+  def parse_grouped_values(values)
+    values = values.split(',').map(&:strip)
+    values.collect do |value|
+      value.gsub(/[\(\)]+/, '')
+    end
+  end
+
+  def handle_group(group)
+    attribute, *values = group.split
+    values = values.join(' ')
+    if values =~ /\(.*?\)/
+      hash.store attribute.to_sym, parse_grouped_values(values)
+    else
+      hash.store attribute.to_sym, Array(values)
+    end
   end
 end
--- test/commands/find_command_test.rb
+++ test/commands/find_command_test.rb
@@ -19,4 +19,32 @@ class FindCommandTest < MiniTest::Test
   def test_it_has_a_usage
     assert_respond_to FindCommand, :usage
   end
+
+  def test_it_can_handle_a_single_attribute_and_condition
+    f = FindCommand.new(nil, 'state CO')
+    expected_state = { state: ['CO'] }
+    assert_equal expected_state, f.parse_args
+
+    f = FindCommand.new(nil, 'zipcode 80021')
+    expected_zipcode = { zipcode: ['80021'] }
+    assert_equal expected_zipcode, f.parse_args
+  end
+
+  def test_it_can_handle_a_single_attribute_and_multiple_conditions
+    f = FindCommand.new(nil, 'state (CO, CA)')
+    expected = { state: ['CO', 'CA'] }
+    assert_equal expected, f.parse_args
+  end
+
+  def test_it_can_handle_multiple_attributes_with_a_single_condition
+    f = FindCommand.new(nil, 'state CO and zipcode 80021')
+    expected = { state: ['CO'], zipcode: ['80021'] }
+    assert_equal expected, f.parse_args
+  end
+
+  def test_it_can_handle_multiple_attributes_with_multiple_conditions
+    f = FindCommand.new(nil, 'state (CO, CA) and zipcode (80021, 92507)')
+    expected = { state: ['CO', 'CA'], zipcode: ['80021', '92507'] }
+    assert_equal expected, f.parse_args
+  end
 end
