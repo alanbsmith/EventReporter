f5b5deadae1f914b81546ce4e69778baea6c1d23
Jonmichael Chambers <jon@tyrbo.net>
2014-06-26 11:06:52 -0600

    Print now shows pages of data rather than the whole collection at once.

--- lib/queue.rb
+++ lib/queue.rb
@@ -1,8 +1,9 @@
 require 'table_print'
 require 'csv'
+require 'io/console'
 
 class Queue
-  attr_reader :data
+  attr_reader :data, :iterations
 
   def initialize
     @data = []
@@ -22,12 +23,36 @@ class Queue
 
   def print_by(attribute)
     sorted_entries = data.sort_by {|entry| entry.send(attribute)  }
-    tp sorted_entries
+    handle_loop(sorted_entries)
+  end
+
+  def handle_loop(sorted_entries)
+    @iterations = 0
+    sorted_entries.each_slice(10) do |slice|
+      break unless display_set(slice)
+    end
+  end
+
+  def display_set(set)
+    @iterations += 1
+    tp set
+    puts "Showing matches: #{((iterations-1) * 10) + 1} - #{iterations*10}", ''
+    if (data.count - (iterations-1) * 10) > 10
+      chr = get_character
+      return false if chr == 'q'
+    end
+    true
+  end
+
+  def get_character
+    puts 'Press Space or Enter to continue. Q to Quit', ''
+    STDIN.getch.downcase
   end
 
   def save(filename)
     headers = [:_, :regdate, :first_name, :last_name, :homephone, :email_address, :street, :city, :state, :zipcode]
-    CSV.open(filename, 'wb', headers: headers, write_headers: true) do |csv|
+    CSV.open(filename, 'wb') do |csv|
+      csv << headers
       data.each do |d|
         csv << [
           d.id, d.regdate, d.first_name, d.last_name, d.homephone,
